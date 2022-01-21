218969e1f86db4ac21597351d9cb76a1788e357a
Luise Schneider <luise.schneider@berlin.de>
2022-01-21 20:49:48 +0100

    add error page for wrong url

--- src/components/PollPage.js
+++ src/components/PollPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 import VoteForm from "./VoteForm";
 import VoteDisplay from "./VoteDisplay";
 
@@ -16,23 +18,32 @@ class PollPage extends Component {
 
     return (
       <div>
-        <h1>Would you rather...</h1>
-        <div className="container-content container-vote">
-          {hasUserAnswered() ? (
-            <VoteDisplay id={question.id} />
-          ) : (
-            <VoteForm id={question.id} onAdd={this.saveAnswer} />
-          )}
-
-          <div className="author">
-            <p>Author: {author.id}</p>
-            <img
-              className="avatar"
-              src={author.avatarURL}
-              alt={`Avatar of user ${author.id}`}
-            />
+        {question === undefined ? (
+          <div className="container-content container-error">
+            <FontAwesomeIcon icon={faExclamationTriangle} />
+            <p>Sorry, this poll doesn't exist.</p>
           </div>
-        </div>
+        ) : (
+          <div>
+            <h1>Would you rather...</h1>
+            <div className="container-content container-vote">
+              {hasUserAnswered() ? (
+                <VoteDisplay id={question.id} />
+              ) : (
+                <VoteForm id={question.id} onAdd={this.saveAnswer} />
+              )}
+
+              <div className="author">
+                <p>Author: {author.id}</p>
+                <img
+                  className="avatar"
+                  src={author.avatarURL}
+                  alt={`Avatar of user ${author.id}`}
+                />
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
@@ -41,7 +52,7 @@ class PollPage extends Component {
 function mapStateToProps({ questions, authedUser, users }, props) {
   const { question_id } = props.match.params;
   const question = questions[question_id];
-  const author = users[question.author];
+  const author = question !== undefined ? users[question.author] : null;
   return { question, authedUser, author };
 }
 
