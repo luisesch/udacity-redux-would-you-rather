ded560a8b5a239933300678dab7ad862d2112137
Luise Schneider <luise.schneider@berlin.de>
2022-01-21 23:43:09 +0100

    improve styling

--- src/components/VoteForm.js
+++ src/components/VoteForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { handleSaveAnswer } from "../actions/questions";
 import { connect } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBalanceScale } from "@fortawesome/free-solid-svg-icons";
 import { OPTIONS } from "../utils/CONSTANTS";
 
 class VoteForm extends Component {
@@ -24,26 +26,37 @@ class VoteForm extends Component {
     const { selectedOption } = this.state;
 
     return (
-      <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
-        {OPTIONS.map((option) => (
-          <div key={option.value} className="container-radio">
-            <input
-              type="radio"
-              id={option.value}
-              value={option.value}
-              name="option"
-            />
-            <label htmlFor={option.value}>{question[option.value].text}</label>
-          </div>
-        ))}
-        <button
-          className="button"
-          type="submit"
-          disabled={selectedOption === ""}
+      <div className="container-with-img">
+        <form
+          onSubmit={this.handleSubmit}
+          onChange={this.handleChange}
+          className="add-form"
         >
-          SUBMIT
-        </button>
-      </form>
+          {OPTIONS.map((option) => (
+            <div key={option.value} className="container-radio">
+              <input
+                type="radio"
+                id={option.value}
+                value={option.value}
+                name="option"
+              />
+              <label htmlFor={option.value}>
+                {question[option.value].text}
+              </label>
+            </div>
+          ))}
+          <button
+            className="button"
+            type="submit"
+            disabled={selectedOption === ""}
+          >
+            SUBMIT
+          </button>
+        </form>
+        <div className="container-icon">
+          <FontAwesomeIcon icon={faBalanceScale} width={"100%"} />
+        </div>
+      </div>
     );
   }
 }
